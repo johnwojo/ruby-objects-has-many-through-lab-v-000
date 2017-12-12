01f04e1735cf369285fae468cca4069b34d8d165
John Wojciechowski <jwojo13@gmail.com>
2017-12-12 21:12:09 +0000

    automatically backed up by learn

--- lib/genre.rb
+++ lib/genre.rb
@@ -8,14 +8,17 @@ class Genre
   end
 
 
+
+    def songs
+      @songs
+    end
+
+
   def add_song(song)
     @songs << song
     song.genre = self
   end
 
-  def songs
-    @songs
-  end
 
   def artists
     @songs.collect do |song|
