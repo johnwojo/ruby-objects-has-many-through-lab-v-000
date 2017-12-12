50b02c442d76489f0d21136d8955eb802f7fa7c0
John Wojciechowski <jwojo13@gmail.com>
2017-12-12 21:18:37 +0000

    automatically backed up by learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -1,8 +1,11 @@
 class Doctor
 
-def initialize(name)
-  @name = name
-  @appointments = []
-end
+  def initialize(name)
+    @name = name
+    @appointments = []
+  end
+
+  def add_appointment
+    
 
 end
